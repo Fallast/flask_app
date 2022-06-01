4d3baed1dcffcc74521b22878f7f56935f97b64e
brian <braianyeah@gmail.com>
2022-06-01 20:36:06 -0300

    progreso en mapeo objeto/relacional, falta asociar tabla product con shipped_products, bought_products etc

--- malabar/models.py
+++ malabar/models.py
@@ -7,17 +7,45 @@ def load_user(user_id):
 	return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
+	__tablename__ = "user"
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(20), unique=True, nullable=False)
 	email = db.Column(db.String(40), unique=True, nullable=False)
 	password = db.Column(db.String(60), nullable=False)
 	# TODO:
-		# relacion bidireccinoal con productos
+		# relacion one-to-many con productos
+	chart_products = db.relationship("Chart_products")
+	bought_products = db.relationship("Bought_products")
+	shipped_products = db.relationship("Shipped_products")
 
 	def __repr__(self):
 		return f"User('{self.username}', '{self.email}', '{self.password}')"
 
+class Chart_products(db.Model):
+	__tablename__ = "chart_products"
+	id = db.Column(db.Integer, primary_key=True)
+	user_id = Column(Integer, db.ForeignKey("user.id"))
+	product = db.relationship("Product")
+	quantity = db.Column(db.Integer)
+
+class Bought_products(db.Model):
+	__tablename__ = "bought_products"
+	id = db.Column(db.Integer, primary_key=True)
+	user_id = Column(Integer, db.ForeignKey("user.id"))
+	product = db.relationship("Product")
+	quantity = db.Column(db.Integer)
+
+class Shipped_products(db.Model):
+	__tablename__ = "shipped_products"
+	id = db.Column(db.Integer, primary_key=True)
+	user_id = Column(Integer, db.ForeignKey("user.id"))
+	product = db.relationship("Product")
+	quantity = db.Column(db.Integer)
+
+# TODO:
+# ruido con ForeignKey, estudiar many to many , dibujar
 class Product(db.Model):
+	__tablename__ = "product"
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(60), unique=True, nullable=False)
 	price = db.Column(db.String(5), nullable=False)
